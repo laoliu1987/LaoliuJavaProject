d78f01ef6a211a98aaf430bb74b953dc231e71d0
老刘 <741038708@qq.com>
2020-09-26 21:23:32 +0800

    Updated on Sep,26th,2020

--- class926/test.java
+++ class926/test.java
@@ -1,15 +1,25 @@
 package class926;
 
+import java.io.FileNotFoundException;
 import java.lang.Throwable;
 public class test {
-    int a=2/0;
 
     public static void main(String[] args) {
-        String[] s =new String[3];
-        System.out.println(s[2]);
-        String st="aaa";
-        if(st.length()>2){
-            throw new StringIndexOutOfBoundsException("String测试异常");
+
+        d();
+
+    }
+        // System.out.println("c2");
+    public static void d(){
+        try{
+            //放置有可能产生出现异常的代码
+//            throw new FileNotFoundException();
+        }catch(Exception e){
+            //当try中产生异常时如何处理异常
+//            System.out.println("--FileNotFoundException--");
+        }finally {
+            //此处代码一定会执行
+            System.out.println("--end--");
         }
     }
 }
