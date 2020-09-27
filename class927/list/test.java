a7e7173d4e592a318b94714b74a718529a36ae0b
老刘 <741038708@qq.com>
2020-09-27 21:54:57 +0800

    Updated on Sep,27th,2020

--- class927/list/test.java
+++ class927/list/test.java
@@ -9,11 +9,20 @@ public class test {
         //往集合中存放数据
         list.add(123);
         list.add("aaaa");
-        list.add(new Object());
+        list.add(1122);
 
         //collection父接口中提供的公共方法
         System.out.println(list.size());
         System.out.println(list.isEmpty());
         System.out.println(list.contains(123));
+//        list.remove(2);
+//        list.contains();
+//        list.containsAll();
+//        list.removeAll();
+//        list.clear();
+        Object j=list.get(0);
+        System.out.println(j);
+        Object j1=list.set(2,1);
+        System.out.println(j1);
     }
 }
