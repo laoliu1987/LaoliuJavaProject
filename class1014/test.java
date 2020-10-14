ffc4493b035ca5a0bca7124379bd25b4f872c56d
老刘 <741038708@qq.com>
2020-10-14 21:39:05 +0800

    Updated on Oct 14th,2020

--- class1014/test.java
+++ class1014/test.java
@@ -5,19 +5,6 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        //国际化 本地化
-        Collator.getInstance(Locale.CHINA).compare("","");
-        List list=new ArrayList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(5);
-        update(list);
-        for(Object o:list){
-            System.out.println(o);
-        }
-    }
-    public static void update(List list){
-        list.remove(new Integer(3));
+
     }
 }
