e0642b353775457d9d7303c097593633348f0ac3
老刘 <741038708@qq.com>
2020-10-16 21:50:40 +0800

    Updated on Oct 16th,2020

---
+++ class1016/user.java
@@ -0,0 +1,20 @@
+package class1016;
+
+public class user {
+    private int id;
+    public user(int id,String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
