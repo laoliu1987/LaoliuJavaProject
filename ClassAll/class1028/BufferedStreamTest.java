99b6b1ea2ee2b07fdd8961f080e23b5bfbd50695
老刘 <741038708@qq.com>
2020-10-28 21:21:53 +0800

    Updated on Oct 28th,2020

--- ClassAll/class1028/BufferedStreamTest.java
+++ ClassAll/class1028/BufferedStreamTest.java
@@ -1,12 +1,27 @@
 package ClassAll.class1028;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class BufferedStreamTest {
     static String path="C:\\py\\bs.txt";
     public static void main(String[] args) {
-        printer();
+//        printer();
+        read();
+    }
+    public static void read(){
+        try {
+            Reader r=new FileReader(path);
+            BufferedReader br =new BufferedReader(r);
+            String str=null;
+            while((str=br.readLine())!=null){
+                System.out.println(str);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
     public static void printer(){
         try {
