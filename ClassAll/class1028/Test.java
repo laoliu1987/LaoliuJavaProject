edc90b01b669e99d12d63d97ac266e2ce3f6e23d
老刘 <741038708@qq.com>
2020-10-28 21:56:53 +0800

    Updated on Oct 28th,2020

--- ClassAll/class1028/Test.java
+++ ClassAll/class1028/Test.java
@@ -1,15 +1,15 @@
 package ClassAll.class1028;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         //标准输入流
-        test1();
+//        test1();
+        test2();
     }
     public static void test1(){
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
@@ -26,5 +26,31 @@ public class Test {
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+    public static void test2(){
+
+        try {
+            FileOutputStream fos =new FileOutputStream("C:\\py\\bs.txt");
+            //创建PrintStream对象
+            PrintStream ps = new PrintStream(fos,true);
+            if(null!=ps){
+                System.setOut(ps);
+            }
+            for(int i=0;i<255;i++){
+                System.out.print((char)i);
+                if(i%50==0){
+                    System.out.println();
+                }
+            }
+            ps.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //此时System.out为null 不会输出东西
+        System.out.println(System.out);
+        System.out.println("123");
     }
 }
